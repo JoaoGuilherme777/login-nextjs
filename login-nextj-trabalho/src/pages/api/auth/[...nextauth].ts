213c26bf8748b0b5fc0d36a89cd5a3205937c3e0
Guilherm <joaogui419121@gmail.com>
2023-01-26 17:52:12 -0300

    trabalho

---
+++ login-nextj-trabalho/src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,13 @@
+import NextAuth from "next-auth/next";
+import GithubProvider from 'next-auth/providers/github';
+import CredentialsProvider from 'next-auth/providers/credentials';
+
+export default NextAuth({
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+  ],
+  secret: process.env.SECRET,
+});
